a84a84f5bd8ca2f4f845b51444df4e119e3fdeeb
Thomas Lee <thomaslee9474@gmail.com>
2019-02-15 23:57:35 -0800

    [update] character route

--- src/api/characters/character.route.js
+++ src/api/characters/character.route.js
@@ -4,16 +4,16 @@ const parcel = require('../../middleware/asyncHandler');
 
 const router = express.Router();
 
-//  GET: returns all user objects in array (includes artists and reg users)
+//  GET: RETURNS LIST OF ALL CHARACTERS
 router.get('/', parcel(controller.Index));
 
-//  GET: returns a specific user object from given Id (reg user or artist)
+// GET: RETURNS SPECIFIC CHARACTER
 router.get('/:id', parcel(controller.Get));
 
-//  DELETE: removes a specific user given Id (includes artists)
+// DELETE: DELETES A SPECIFIC CHARACTER
 router.delete('/:id', parcel(controller.Delete));
 
-//  PATCH: updates a users information (includes artist)
+// PATCH: UPDATE CHARACTER INFO
 router.patch('/:id', parcel(controller.Update));
 
 module.exports = router;
