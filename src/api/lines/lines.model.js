52bf0fd20b9553185ad0b95ae588c948cf824b96
Thomas Lee <thomaslee9474@gmail.com>
2019-02-15 19:55:49 -0800

    [added] files

---
+++ src/api/lines/lines.model.js
@@ -0,0 +1,23 @@
+// SOUTHPARK 'LINES' MODEL
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema; 
+
+const LineSchema = new Schema({
+    season: {
+        1: {
+            episode: 
+        }
+    }
+    lines: {type: String, required: true}
+});
+
+const character = mongoose.model('Character', CharacterSchema); 
+
+module.exports = {
+  Lines
+};
+
+{
+    lines: "",
+
+}
