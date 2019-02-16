45b11fbddc855bf59aae514576a6fbed2bad457d
Thomas Lee <thomaslee9474@gmail.com>
2019-02-16 13:54:36 -0800

    [changes]

--- src/api/characters/character.controller.js
+++ src/api/characters/character.controller.js
@@ -2,22 +2,22 @@ const Model = require('./character.model');
 
 // RETURNS LIST OF ALL CHARACTERS
 async function Index(req, res) {
-  res.send(await Model.Character.find());
+  res.json(await Model.Character.find());
 }
 
 //  SEND SPECIFIC CHARACTER
 async function Get(req, res) {
-  res.send(await Model.Character.findById(req.params.id));
+  res.json(await Model.Character.findById(req.params.id));
 }
 
 //  DELETE SPECIFIC CHARACTER
 async function Delete(req, res) {
-  res.send(await Model.Character.findOneAndRemove({ _id: req.params.id }));
+  res.json(await Model.Character.findOneAndRemove({ _id: req.params.id }));
 }
 
 //  UPDATE SPECIFIC CHARACTER
 async function Update(req, res) {
-  res.send(await Model.Character.findOneAndUpdate({ _id: req.params.id }, req.body));
+  res.json(await Model.Character.findOneAndUpdate({ _id: req.params.id }, req.body));
 }
 
 module.exports = {
