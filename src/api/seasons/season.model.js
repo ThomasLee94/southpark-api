b1ccbd81e690765b8db9966cccea19400ba49d4c
Thomas Lee <thomaslee9474@gmail.com>
2019-02-17 13:44:47 -0800

    [added] sinon unit test @Connor-Cahill

--- src/api/seasons/season.model.js
+++ src/api/seasons/season.model.js
@@ -5,7 +5,6 @@ const Schema = mongoose.Schema;
 const SeasonSchema = new Schema({
   seasonNumber: { type: Number, required: true}, 
   totalEpisodes: { type: Number, required: true }, 
-  
 });
 
 const Season = mongoose.model('Season', SeasonSchema); 
