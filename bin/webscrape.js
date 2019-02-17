995dcf7e64f0434cd9d1832deeebd02701710e1f
Thomas Lee <thomaslee9474@gmail.com>
2019-02-17 15:27:25 -0800

    [update] webscrape

--- bin/webscrape.js
+++ bin/webscrape.js
@@ -7,15 +7,12 @@ require('dotenv').config()
 const Nightmare = require('nightmare');
 const nightmare = Nightmare({ show: true });
 const cheerio = require('cheerio');
-const Scholarship = require('../models/scholarship');
-require('./db/scholarboard-db');
+const Character = require('../source/api/characters/character.model');
+require('./db/southpark-db');
 
+// URL FOR ALL CHARACTERS
 let url = 'http://wiki.southpark.cc.com/wiki/List_of_Characters'
 // # TODO: ASK DANI IF YOU CAN LOOP THROUGH AN ARRAY WITH .togo WITH NIGHTMARE
-// let url = [
-//   'https://www.scholarships.com/financial-aid/college-scholarships/scholarships-by-major/accounting-scholarships/%C2%A1adelante-fund-millercoors-colorado-scholarship/',
-
-// ]
 
 nightmare
   .goto(url)
@@ -26,19 +23,13 @@ nightmare
     // LOADING HTML
     const $ = cheerio.load(result);
 
-    // VARIBALES THAT NEED TO BE ADDED TO THE MODEL
-    // const scholName = $('#innercontent nth-child(2)').text();
     const characterName = $('.character').children().text();
     
 
     // SAVE TEXT AS PROPERTY OF RESULT OBJ
-    const result_obj = {
-      name: scholName,
-      deadline: scholDeadline,
-      funding: scholFunding,
-      contactInfo: scholContact,
-      requirements: scholRequirements,
-    };
+    // const result_obj = {
+    //   name: characterName,
+    // };
 
     const scholarship = new Scholarship(result_obj);
     scholarship.save()
