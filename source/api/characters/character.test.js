198b64716a0ea79e1d989322a7559b6e61628036
Thomas Lee <thomaslee9474@gmail.com>
2019-02-17 23:15:49 -0800

    [update] tests

--- source/api/characters/character.test.js
+++ source/api/characters/character.test.js
@@ -1,10 +1,14 @@
 const mocha = require('mocha');
 const chai = require('chai');
-const utils = require('../utils');
 const server = require('../../index');
 const expect = chai.expect;
 const { assert } = require('chai')
 const agent = chai.request.agent(server);
+const characterNames = require('../../../bin/db/southparkNames');
+
+const sampleCharacter = {
+  
+}
 
 it('should return list of all characters', (done) => {
   agent
@@ -13,13 +17,7 @@ it('should return list of all characters', (done) => {
       if (err) { done(err) }
       res.status.should.be.equal(200);
       res.characters.should.be.an('Array');
-      res.characters.should.include('Eric Cartman');
-      res.characters.should.include('Kyle Broflovski');
-      res.characters.should.include('Stan Marsh');
-      res.characters.should.include('Kenny McCormick');
-      res.characters.should.include('Butter');
-      res.characters.should.include('Kyle');
-
+      res.characters.should.include(characterNames);
       return done();
   });
 });
@@ -31,43 +29,7 @@ it('should return character first name', (done) => {
       if (err) { done(err) }
       res.status.should.be.equal(200);
 
+
     })
 });
 
-it('should return the area of a 5 by 6 rectangle', function() {
-  const area = utils.area(5, 6);
-  expect(area).to.be.a('Number');
-  expect(area).to.equal(30);
-});
-
-it('should return the area of a circle of radius 5', function() {
-  const area = utils.circleArea(5)
-  expect(area).to.be.a('Number')
-  expect(area).to.equal(78.53981633974483)
-});
-
-it('Should create a new (object) Item with name and price', function(){
-  const item = utils.makeObj('name', 'price');
-  assert.isObject(item)
-  expect(item).to.contain.keys('name');
-
-  
-});
-
-it('Should return an array containing all items in cart');
-
-it('Should add a new item to the shopping cart');
-
-it('Should return the number of items in the cart');
-
-it('Should remove items from cart');
-
-// ========================================================
-// Stretch Challenges
-// ========================================================
-
-it('Should update the count of items in the cart');
-
-it('Should validate that an empty cart has 0 items');
-
-it('Should return the total cost of all items in the cart');
