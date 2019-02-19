de6cd24b3f9d4b6e7aade737f449731c8e63fcd1
Thomas <thomaslee9474@gmail.com>
2019-02-19 15:35:08 -0800

    [update] models

--- source/api/lines/lines.model.js
+++ source/api/lines/lines.model.js
@@ -11,7 +11,6 @@ const LineSchema = new Schema({
   character: { type: String, required: true },
   characterId: [{ type: Schema.Types.ObjectId, ref: 'Character' }], 
   episodeNum: [{ type: Schema.Types.ObjectId, ref: 'Episode' }], 
-  line: { type: String, required: true },
 });
 
 const Line = mongoose.model('Line', LineSchema); 
