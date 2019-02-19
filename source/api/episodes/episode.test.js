0540a5f02f2dc98aa8aaba428dc2609e8b1f23c7
Thomas Lee <thomaslee9474@gmail.com>
2019-02-18 16:48:29 -0800

    [update] lint

--- source/api/episodes/episode.test.js
+++ source/api/episodes/episode.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const mocha = require('mocha');
 const chai = require('chai');
 const server = require('../../index');
@@ -9,30 +10,31 @@ it('should return all the episodes of associated season', (done) => {
   agent
     .get('/api/:season/episodes')
     .end((err, res) => {
-      if (err) { done(err) }
+      if (err) { done(err); }
       res.status.should.be.equal(200);
-      res.characters.should.be.an('Array');
-      res.characters.should.include(characterFullNames);
+      // TEST BODY
       return done();
-  });
+    });
 });
 
 it('should return specific episode of associated season', (done) => {
   agent
     .get('/api/:season/:episode')
     .end((err, res) => {
-      if (err) { done(err) }
+      if (err) { done(err); }
+      res.status.should.be.equal(200);
       // TEST BODY
       return done();
-    })
+    });
 });
 
 it('should return length of episode in minutes of associated season', (done) => {
   agent
     .get('/api/:season/:episode/length')
     .end((err, res) => {
-      if (err) { done(err) }
+      if (err) { done(err); }
+      res.status.should.be.equal(200);
       // TEST BODY
-      return done(); 
-    })
-})
+      return done();
+    });
+});
