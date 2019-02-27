c5d11d93973bdfb1e035f18f10b6d0223d3665d3
Thomas Lee <thomaslee9474@gmail.com>
2019-02-26 23:55:31 -0800

    [update] helper funcs

--- bin/util/parse.js
+++ bin/util/parse.js
@@ -5,28 +5,23 @@
 /* eslint-disable func-names */
 /* eslint-disable camelcase */
 
+/*
+Cleans data to a form that can be used
+Example: '301 edit'
+  => {seasonNum: 03, episodeNum: 01}
+*/
+
 module.exports = {
-  /*
-  Cleans data to a form that can be used
-  Example: '301 edit'
-    => {seasonNum: 03, episodeNum: 01}
-  */
-  cleanUpSeas: (string) => {
+  cleanUpSeason: (string) => {
     const stripped_text = string.replace(':', '');
-    const outputString = [];
-    
+    const seasonNum = stripped_text.splice(0, 2);
+    return seasonNum;
   },
 
-  /* Cleans up a body of text from special characters
-    @param - stringArray : An array of strings to be cleaned up
-    @return - clean_strings : An array of cleaned up string in order they were passed
-  */
   cleanUpEpisode: (string) => {
-    let clean_strings = [];
-    stringArray.forEach((string) => {
-      clean_strings.push(string.replace(/(\t\n|\n|\t)/gm, ''))
-    });
-    return clean_strings;
+    const stripped_text = string.replace(':', '');
+    const episodeNum = stripped_text.splice(2, 2);
+    return episodeNum;
   },
 
 };
