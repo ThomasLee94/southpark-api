a2f4e1abd55b2c31b8efb2a047f05d0f3e86f0ec
Thomas <thomaslee9474@gmail.com>
2019-04-09 18:47:14 -0700

    [update] to-do

--- models/episode.model.go
+++ models/episode.model.go
@@ -1,11 +1,11 @@
 package models
 
 type Episode struct {
-  ID            bson.ObjectId `json:"_id,omitempty" bson:"_id,omitempty"`
-  EpisodeName   string        `json:"episodeName,omitempty" bson:"episodeName,omitempty"`
+  ID            bson.ObjectId     `json:"_id,omitempty" bson:"_id,omitempty"`
+  EpisodeName   string            `json:"episodeName,omitempty" bson:"episodeName,omitempty"`
   // TODO: FINISH
-  EpisodeNumber int           `min: "1", max: "30", required: "true"`
-	SeasonNumber  int           `min: "1", max: "25", required: "true"`
-  CharacterId   []string      [{ type: Schema.Types.ObjectId, ref: 'Character' }],
-  LineId:       []string      [{ type: Schema.Types.ObjectId, ref: 'Line' }],
+  EpisodeNumber int               `min: "1", max: "30", required: "true"`
+	SeasonNumber  int               `min: "1", max: "25", required: "true"`
+  CharacterId   []bson.ObjectId   'bson:"_id,omitempty" json:"object'
+  LineId:       []bson.ObjectId   [{ type: Schema.Types.ObjectId, ref: 'Line' }],
 }
