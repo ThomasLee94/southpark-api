7e4342bd7404410473ba933f12681c852e1e04fd
Thomas <thomaslee9474@gmail.com>
2019-04-13 15:52:13 -0700

    [update] db

--- db/sqlite.go
+++ db/sqlite.go
@@ -1,8 +1,29 @@
-package sqlite
+package sqliteDB
 
-import "github.com/jinzhu/gorm"
+import (
+	"fmt"
 
-func main() {
-	db, err := gorm.Open("sqlite3", "/tmp/gorm.db")
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+var db *gorm.DB
+var err error
+
+type Line struct {
+	gorm.Model
+	Character string
+	Lines     string
+}
+
+func InitialMigration() {
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("failed to connect to database")
+
+	}
 	defer db.Close()
+
+	db.AutoMigrate(&Line{})
 }
