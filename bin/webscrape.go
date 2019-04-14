f4edf4f64c64fe7248537be418250aeef2eea980
ThomasLee94 <thomaslee9474@gmail.com>
2019-04-13 22:43:01 -0700

    [added] urls

--- bin/webscrape.go
+++ bin/webscrape.go
@@ -1,1 +1,11 @@
-asdf
+package webscrape
+
+func scrape() {
+	epId := []Episode.EpisodeID
+	lineId := []Line.LineID
+	characterId := []Character.CharacterID
+
+	// DOMAIN TO BE SCRAPED
+	c := 
+}	
+
